4c9529cd37028f4a5193b30451937f1edc66bd43
Miguel <miguelarrieta02@gmail.com>
2021-08-04 11:51:25 +0100

    Removed redundant main() function

--- pdf_merger.py
+++ pdf_merger.py
@@ -9,9 +9,5 @@ def pdf_merger(pdf_list: list):
     merger.write('super.pdf')
 
 
-def main():
-    pdf_merger(sys.argv[1:])
-
-
 if __name__ == '__main__':
-    main()
+    pdf_merger(sys.argv[1:])
